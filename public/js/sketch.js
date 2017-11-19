3066e7014b0d9c0899395e3ace3633fc873e3a29
Nout Kleef <noutkleef1@gmail.com>
2017-11-19 19:36:38 +0100

    Added headers

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -5,7 +5,7 @@ let imageWidth = 240;
 let imageHeight = 120;
 let imageMargin = 15;
 let headerHeight = 50;
-let headerMargin = 20;
+let headerMargin = 10;
 
 // called once, when page is ready
 function setup() {
@@ -20,6 +20,14 @@ function setup() {
 // called Math.min(frameRate(), 60) times / second
 function draw() {
 	background(51);
+	// display headers
+	textSize(headerHeight * 0.6);
+	textAlign(CENTER);
+	noStroke();
+	fill(217);
+	text("input", imageMargin + imageWidth * 0.5, headerHeight * 0.8);
+	text("output", imageMargin * 3 + imageWidth * 1.5, headerHeight * 0.8);
+	// display images
 	inputImage.show();
 	outputImage.show();
 }
