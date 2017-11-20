86f4f4959944a00a7cf74cc45f0234f0afbe65c4
Nout Kleef <noutkleef1@gmail.com>
2017-11-20 19:42:17 +0100

    Added private key validation regex

--- public/js/PODAST/variables.js
+++ public/js/PODAST/variables.js
@@ -1,3 +1,9 @@
 // VARIABLES USED ACROSS GITHUB AND OTHER PODAST PROJECTS
 
 const RGB = [8, 8, 8];
+
+/* use this to extract private key attributes, or to validate
+ * example:
+ * P#i97#d2#p3 --> i: 97, d: 2, p: 3
+ */
+const privateKey = /P#i(\d{1,7})#d([1-9]|(?:1\d)|(?:2[0-3]))#p([1-9]|(?:1\d)|(?:2[0-3]))/;
