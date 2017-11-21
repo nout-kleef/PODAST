afd6bf97b253cbdd7fed6f8e4cb109a82db3ce7e
Nout Kleef <noutkleef1@gmail.com>
2017-11-21 19:52:59 +0100

    Programmed encryption algorithm

--- public/js/PODAST/PODASTImage.js
+++ public/js/PODAST/PODASTImage.js
@@ -29,7 +29,7 @@ PODASTImage.prototype.encrypt = function(plaintext, i, p, d) {
 	Object.assign(newImage, this);
 	// divide the plaintext binary into small portions, ready to be divided over the to be altered pixels
 	let dataPortions = plaintext.match(new RegExp("[01]{1," + d + "}", "g")); // "0111001" --> ["01", "11", "00", "1"] (example I)
-	// add bits indicator (see example II)
+	// add bits indicator
 	dataPortions.push(addLeadingZeroes(decimalToBinary(dataPortions[dataPortions.length - 1].length), d));
 	if(DEBUGGING >= 3) {
 		console.log("data portions: ", dataPortions);
@@ -38,65 +38,65 @@ PODASTImage.prototype.encrypt = function(plaintext, i, p, d) {
 	let pixelIndex = i;
 	let previousPixel;
 	const pixelsRange = Math.pow(2, p); // look for pixels 0 to pixelsRange pixels from current
-	for(var m = 0; m < dataPortions.length; m++) {
-		let currentPortion = dataPortions[m];
-		let nextPortion;
-		let relativeNextIndex;
-		let currentPixel = this.pixels[pixelIndex];
-		// update the data
-		currentPixel.binaryData = currentPortion;
-		if(m === dataPortions.length - 2) {
-			/* This is the final iteration of actual data
-			 * We need to check how many bits the last portion contains,
-			 * because otherwise there is no way to tell if "01" or "1" was encrypted
-			 * when we're decrypting a pixel.
-			 * To solve this, we have to do perform 2 steps:
-			 * 1: include a 00..(p times) terminator (0-terminator) in the final/nth pixel, to let the 
-			 * 	decryption algorithm know it should not continue to the next pixel, because the entire 
-			 * 	string has been decrypted.
-			 * 2: in the nth pixel, we can use the freed up space (as we don't have to hide anything)
-			 * 	for telling the algorithm which bits in the data part of the (n-1)th pixel are actual data,
-			 * 	and which aren't.
-			 * 	We do this by using the _value_ of the data part of the nth pixel to tell _how many_ bits
-			 * 	in the (n-1)th pixel were actual data.
-			 * 	For example: (example II)
-			 * 		n-1: "01", n: "10" would mean: 2 ("10") of the data bits in (n-1) are actual data
-			 * 		n-1: "10111", n: "01100" would mean: 4 ("00100") of the data bits in (n-1) are actual data,
-			 * 			so the first 2 characters of "10111" should be discarded.
-			 * 	That's why we add that final portion to the dataPortions element. (see note I)
-			 */
-		} else if(m === dataPortions.length - 1) {
-		} else {
-			nextPortion = dataPortions[m + 1];
-			// find next pixel
-			relativeNextIndex = getNextPixelIndex(nextPortion, pixelIndex, pixelsRange, p, d);
-			if(relativeNextIndex === false) {
-				if(DEBUGGING >= 1) {
-					console.error("Could not encrypt with the following parameters; i: " + 
-						i + ", p: " + p + ", d: " + d + ". Please retry with different parameters.");
-				}
-				return;
+	// NOTE we are altering newImage, not this
+	for(var m = 0; m < dataPortions.length - 1; m++) {
+		const currentPixel = newImage.pixels[pixelIndex];
+		const currentData = dataPortions[m];
+		const nextData = dataPortions[m + 1];
+		const currentPointer = 0;
+		// update data for current pixel
+		currentPixel.binaryData = currentData;
+		const relativeNextPixelIndex = newImage.getNextPixelIndex(nextData, pixelIndex, p, d);
+		if(relativeNextPixelIndex === false) {
+			if(DEBUGGING >= 1) {
+				console.warn("PODAST was unable to complete the encryption process with the following \n" +
+					"parameters: i=" + i + ", p=" + p + ", d=" + d + ", because it couldn't find any unaltered \n" +
+					"pixels within " + pixelsRange + " pixels from the current pixel.\n" +
+					"Please retry with different parameters, different text or a different image.");
 			}
-			currentPixel.binaryPointer = addLeadingZeroes(decimalToBinary(relativeNextIndex), p);
+			return false;
+		} else {
+			if(m === dataPortions.length - 2) {
+				// possible bug: should be modularized or not?
+				const nextPixelIndex = pixelIndex + relativeNextPixelIndex;
+				let nextPixel = newImage.pixels[nextPixelIndex];
+				// update data in advance
+				nextPixel.binaryData = nextData;
+				// because that pixel will be the last, we can safely set the pointer to 0-terminator
+				nextPixel.binaryPointer = addLeadingZeroes(0, p);
+				// done!
+				if(DEBUGGING >= 1) {
+					console.info("Success! PODAST successfully encrypted your data with the following parameters: \n" +
+						"i=" + i + ", p=" + p + ", d=" + d + ". Want to share your encrypted image? First, download your \n" +
+						"encrypted image, and send it to the receiver. Make sure you also share your decryption key: \n" +
+						"%c" + generatePrivateKey(i, p, d) + "%c , or else the receiver won't be able to read your message.", "font-weight: 900", "font-weight: initial");
+				}
+				break;
+			}
+			// update pointer for current pixel
+			currentPixel.binaryPointer = addLeadingZeroes(decimalToBinary(relativeNextPixelIndex), p);
 		}
-		pixelIndex += relativeNextIndex;
-		previousPixel = currentPixel;
+		pixelIndex = nextPixelIndex;
 	}
+	return newImage;
 };
 
 PODASTImage.prototype.getNextPixelIndex = function(data, index, lookahead, p, d) {
 	let response = false;
 	for(var i = index; i < index + lookahead; i++) {
-		if(this.pixels[i].isAltered()) {
-			// can't alter it twice
+		// TODO optimize?
+		const actualIndex = i % this.pixels.length;
+		if(this.pixels[actualIndex].isAltered()) {
+			// can't alter it twice, would corrupt existing data
 			continue;
 		} else {
-			const currentPixelData = this.pixels[i].getSignificanceArray().slice(-p-d, -p);
-			if(currentPixelData === data || i === index + lookahead - 1) {
-				if(i === index + lookahead - 1 && DEBUGGING >= 2) {
-					console.info("Unable to find pixel that already contained the data we're trying to hide, using last pixel in range instead.");
-				}
-				response = i - index;
+			const authenticPixelData = this.pixels[actualIndex].getSignificanceArray().slice(-p-d, -p);
+			if(authenticPixelData === data) {
+				response = actualIndex - index;
+				break;
+			} else if(actualIndex === index + lookahead - 1 && DEBUGGING >= 2) {
+				console.info("Unable to find pixel that already contained the data we're trying to hide, using last pixel in range instead.");
+				response = actualIndex - index;
 				break;
 			}
 		}
--- public/js/PODAST/variables.js
+++ public/js/PODAST/variables.js
@@ -6,4 +6,10 @@ const RGB = [8, 8, 8];
  * example:
  * P#i97#d2#p3 --> i: 97, d: 2, p: 3
  */
-const privateKey = /P#i(\d{1,7})#d([1-9]|(?:1\d)|(?:2[0-3]))#p([1-9]|(?:1\d)|(?:2[0-3]))/;
+const validatePrivateKeyRegEx = /P#i(\d{1,7})#d([1-9]|(?:1\d)|(?:2[0-3]))#p([1-9]|(?:1\d)|(?:2[0-3]))/;
+
+/* use this to generate a private key given i, p, d
+ * example:
+ * i: 97, d: 2, p: 3 --> P#i97#d2#p3
+ */
+const generatePrivateKey = (i, p, d) => "P#i" + i + "#d" + d + "#p" + p;
