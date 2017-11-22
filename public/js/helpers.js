8070d1431787d1267a0ad86010d5813a4ebb0ec4
Nout Kleef <noutkleef1@gmail.com>
2017-11-22 10:54:23 +0100

    Test1

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -1,6 +1,6 @@
 const decimalToBinary = (decimal) => (decimal >>> 0).toString(2);
 
-const binaryToDecimal = (binary) => parseInt(binary, 2);
+const binaryToDecimal = (binary) => parseInt(binary, 2); 
 
 // add leading zeroes
 // NOTE: only works for up to 8 characters
